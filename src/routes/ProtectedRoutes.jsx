61fe2eef52eb0db67a7295e9d7036486cfd636c9
mosesonuh <ichadomoses@gmail.com>
2024-08-15 16:29:43 +0100

    remove chakra ui dependencies

--- src/routes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes.jsx
@@ -1,25 +1,25 @@
 import { Outlet, useNavigate, useLocation, Navigate, NavLink } from "react-router-dom";
 import { useEffect, useRef, useState } from "react";
-import {
-  ChakraProvider,
-  useDisclosure,
-  Button,
-  Input,
-  Text,
-  Box,
-  Flex,
-} from "@chakra-ui/react";
-import { toast } from "react-toastify";
+// import {
+//   ChakraProvider,
+//   useDisclosure,
+//   Button,
+//   Input,
+//   Text,
+//   Box,
+//   Flex,
+// } from "@chakra-ui/react";
 
-import {
-  Drawer,
-  DrawerBody,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerCloseButton,
-} from "@chakra-ui/react";
+// import {
+//   Drawer,
+//   DrawerBody,
+//   DrawerFooter,
+//   DrawerHeader,
+//   DrawerOverlay,
+//   DrawerContent,
+//   DrawerCloseButton,
+// } from "@chakra-ui/react";
+import { toast } from "react-toastify";
 import useAuth from "../hooks/useAuth";
 import { accessToken, refreshToken } from "../utils/constants";
 import Loader from "../components/loading/Loading";
@@ -109,12 +109,6 @@ function SideBar() {
 
   return (
     <nav className="bg-indigo-700 px-2 flex py-3 justify-between sm:px-10">
-      {/* <NavLink
-              to="/"
-              className="md:text-2xl text-white hover:bg-gray-900 px-3 py-2 rounded-lg"
-            >
-              Building Manager
-            </NavLink> */}
       <div></div>
       <div className="nav-items gap-x-2 flex md:gap-x-4">
         <NavLink
@@ -134,117 +128,85 @@ function SideBar() {
         </button>
       </div>
     </nav>
-    // <ChakraProvider>
-    //   <Box pl="4">
-    //     <Button ref={btnRef} onClick={onOpen}>
-    //       <FaBars />
-    //     </Button>
-    //     <Drawer
-    //       isOpen={isOpen}
-    //       placement="left"
-    //       onClose={onClose}
-    //       finalFocusRef={btnRef}
-    //     >
-    //       <DrawerOverlay />
-    //       <DrawerContent>
-    //         <DrawerCloseButton />
-    //         <DrawerBody>
-    //           {/* <button onClick={navigateToProjectsPage}>Projects</button>
-    //             <button>log Out</button> */}
-    //           <p onClick={navigateToProjectsPage}>Projects</p>
-    //           <p onClick={navigateToProjectInvitesPage}>Invites</p>
-    // <p onClick={logOut}>Log Out</p>
-    //           {/* <p>Some contents...</p> */}
-    //         </DrawerBody>
-
-    //         <DrawerFooter>
-    //           <Button variant="outline" mr={3} onClick={onClose}>
-    //             Cancel
-    //           </Button>
-    //         </DrawerFooter>
-    //       </DrawerContent>
-    //     </Drawer>
-    //   </Box>
-    // </ChakraProvider>
   );
 }
-function SideBarOld() {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const btnRef = useRef();
-  const { error, setErrToNull, post } = useAPI();
-  const { setAuth } = useAuth();
+// function SideBarOld() {
+//   const { isOpen, onOpen, onClose } = useDisclosure();
+//   const btnRef = useRef();
+//   const { error, setErrToNull, post } = useAPI();
+//   const { setAuth } = useAuth();
 
-  const navigate = useNavigate();
+//   const navigate = useNavigate();
 
-  const navigateToProjectsPage = () => {
-    navigate("/projects");
-  };
-  const navigateToProjectInvitesPage = () => {
-    navigate("/invites");
-  };
-  const navigateToLoginPage = () => {
-    navigate("/login");
-  };
+//   const navigateToProjectsPage = () => {
+//     navigate("/projects");
+//   };
+//   const navigateToProjectInvitesPage = () => {
+//     navigate("/invites");
+//   };
+//   const navigateToLoginPage = () => {
+//     navigate("/login");
+//   };
 
-  const toastId = useRef(null);
-  const logOut = async () => {
-    try {
-      toastId.current = toast.loading("Logging out...");
-      // const response = await post(`/User/logout`);
-      await post(`/User/logout`);
-      sessionStorage.removeItem(accessToken);
-      sessionStorage.removeItem(refreshToken);
-      setAuth({});
-      toast.update(toastId.current, {
-        render: "Logout Successful",
-        type: "success",
-        isLoading: false,
-      });
-      setTimeout(() => toast.dismiss(), 3000);
-      setErrToNull();
-      navigateToLoginPage;
-    } catch (err) {
-      toast.update(toastId.current, {
-        render: error?.message || "Error Signing out",
-        type: "error",
-        isLoading: false,
-      });
-      setTimeout(() => toast.dismiss(), 3000);
-    }
-  };
+//   const toastId = useRef(null);
+//   const logOut = async () => {
+//     try {
+//       toastId.current = toast.loading("Logging out...");
+//       // const response = await post(`/User/logout`);
+//       await post(`/User/logout`);
+//       sessionStorage.removeItem(accessToken);
+//       sessionStorage.removeItem(refreshToken);
+//       setAuth({});
+//       toast.update(toastId.current, {
+//         render: "Logout Successful",
+//         type: "success",
+//         isLoading: false,
+//       });
+//       setTimeout(() => toast.dismiss(), 3000);
+//       setErrToNull();
+//       navigateToLoginPage;
+//     } catch (err) {
+//       toast.update(toastId.current, {
+//         render: error?.message || "Error Signing out",
+//         type: "error",
+//         isLoading: false,
+//       });
+//       setTimeout(() => toast.dismiss(), 3000);
+//     }
+//   };
 
-  return (
-    <ChakraProvider>
-      <Box pl="4">
-        <Button ref={btnRef} onClick={onOpen}>
-          <FaBars />
-        </Button>
-        <Drawer
-          isOpen={isOpen}
-          placement="left"
-          onClose={onClose}
-          finalFocusRef={btnRef}
-        >
-          <DrawerOverlay />
-          <DrawerContent>
-            <DrawerCloseButton />
-            <DrawerBody>
-              {/* <button onClick={navigateToProjectsPage}>Projects</button>
-                <button>log Out</button> */}
-              <p onClick={navigateToProjectsPage}>Projects</p>
-              <p onClick={navigateToProjectInvitesPage}>Invites</p>
-              <p onClick={logOut}>Log Out</p>
-              {/* <p>Some contents...</p> */}
-            </DrawerBody>
+//   return (
+//     <ChakraProvider>
+//       <Box pl="4">
+//         <Button ref={btnRef} onClick={onOpen}>
+//           <FaBars />
+//         </Button>
+//         <Drawer
+//           isOpen={isOpen}
+//           placement="left"
+//           onClose={onClose}
+//           finalFocusRef={btnRef}
+//         >
+//           <DrawerOverlay />
+//           <DrawerContent>
+//             <DrawerCloseButton />
+//             <DrawerBody>
+//               {/* <button onClick={navigateToProjectsPage}>Projects</button>
+//                 <button>log Out</button> */}
+//               <p onClick={navigateToProjectsPage}>Projects</p>
+//               <p onClick={navigateToProjectInvitesPage}>Invites</p>
+//               <p onClick={logOut}>Log Out</p>
+//               {/* <p>Some contents...</p> */}
+//             </DrawerBody>
 
-            <DrawerFooter>
-              <Button variant="outline" mr={3} onClick={onClose}>
-                Cancel
-              </Button>
-            </DrawerFooter>
-          </DrawerContent>
-        </Drawer>
-      </Box>
-    </ChakraProvider>
-  );
-}
+//             <DrawerFooter>
+//               <Button variant="outline" mr={3} onClick={onClose}>
+//                 Cancel
+//               </Button>
+//             </DrawerFooter>
+//           </DrawerContent>
+//         </Drawer>
+//       </Box>
+//     </ChakraProvider>
+//   );
+// }
